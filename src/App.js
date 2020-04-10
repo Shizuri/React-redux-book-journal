7f2d85ea3d8032b402845bdc4bd69018c0ff6786
Zdravko Mavkov <mavkov@hotmail.com>
2020-04-10 13:06:36 +0200

    Converted App.js and JournalEntry.js to class components

--- src/App.js
+++ src/App.js
@@ -1,94 +1,95 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import BookBrowser from './BookBrowser'
 import BookDetails from './BookDetails'
 import Journal from './Journal'
 import JournalEntryDetails from './JournalEntryDetails'
 import EditJournalEntry from './EditJournalEntry'
-import { NavLink, Route, Switch, Redirect, useHistory } from 'react-router-dom'
+import { NavLink, Route, Switch, Redirect, withRouter } from 'react-router-dom'
 
-const App = props => {
-	// The code here is for the purpose of swipe navigation on mobile devices
-	const history = useHistory()
-
-	// Swipe Up / Down / Left / Right
-	let initialX = null
-	let initialY = null
-
-	const startTouch = e => {
-		initialX = e.touches[0].clientX
-		initialY = e.touches[0].clientY
-	}
-
-	const moveTouch = e => {
-		if (!initialX || !initialY) {
-			return
+class App extends Component {
+	render() {
+		// The code here is for the purpose of swipe navigation on mobile devices
+	
+		// Swipe Up / Down / Left / Right
+		let initialX = null
+		let initialY = null
+	
+		const startTouch = e => {
+			initialX = e.touches[0].clientX
+			initialY = e.touches[0].clientY
 		}
-
-		const currentX = e.touches[0].clientX
-		const currentY = e.touches[0].clientY
-
-		const diffX = initialX - currentX
-		const diffY = initialY - currentY
-
-		if (Math.abs(diffX) > Math.abs(diffY)) {
-			// sliding horizontally
-			if (diffX > 0) {
-				// swiped left
-				if (history.location.pathname === '/book-browser') {
-					history.push('/journal')
+	
+		const moveTouch = e => {
+			if (!initialX || !initialY) {
+				return
+			}
+	
+			const currentX = e.touches[0].clientX
+			const currentY = e.touches[0].clientY
+	
+			const diffX = initialX - currentX
+			const diffY = initialY - currentY
+	
+			if (Math.abs(diffX) > Math.abs(diffY)) {
+				// sliding horizontally
+				if (diffX > 0) {
+					// swiped left
+					if (this.props.history.location.pathname === '/book-browser') {
+						this.props.history.push('/journal')
+					}
+				} else {
+					// swiped right
+					if (this.props.history.location.pathname === '/journal') {
+						this.props.history.push('/book-browser')
+					}
 				}
 			} else {
-				// swiped right
-				if (history.location.pathname === '/journal') {
-					history.push('/book-browser')
+				// sliding vertically
+				if (diffY > 0) {
+					// swiped up
+				} else {
+					// swiped down
 				}
 			}
-		} else {
-			// sliding vertically
-			if (diffY > 0) {
-				// swiped up
-			} else {
-				// swiped down
-			}
+	
+			initialX = null
+			initialY = null
+	
+			e.preventDefault()
 		}
-
-		initialX = null
-		initialY = null
-
-		e.preventDefault()
+	
+		document.addEventListener('touchstart', startTouch, false)
+		document.addEventListener('touchmove', moveTouch, false)
+	
+		return (
+			<div className='App'>
+				<nav className='App-nav'>
+					<NavLink exact to='/book-browser' activeClassName='App-nav-active' className='App-nav-link'>Book Browser</NavLink>
+					<NavLink exact to='/journal' activeClassName='App-nav-active' className='App-nav-link'>Book Journal</NavLink>
+				</nav>
+	
+				<Switch>
+					<Route exact path='/book-browser'>
+						<BookBrowser />
+					</Route>
+					<Route exact path='/journal'>
+						<Journal />
+					</Route>
+					<Route exact path={'/book-browser/:bookId'}>
+						<BookDetails />
+					</Route>
+					<Route exact path={'/journal/:bookId'}>
+						<JournalEntryDetails />
+					</Route>
+					<Route exact path={'/journal/edit/:bookId'}>
+						<EditJournalEntry />
+					</Route>
+					<Redirect exact from='/' to='/book-browser' />
+				</Switch>
+			</div>
+		)
 	}
-
-	document.addEventListener('touchstart', startTouch, false)
-	document.addEventListener('touchmove', moveTouch, false)
-
-	return (
-		<div className='App'>
-			<nav className='App-nav'>
-				<NavLink exact to='/book-browser' activeClassName='App-nav-active' className='App-nav-link'>Book Browser</NavLink>
-				<NavLink exact to='/journal' activeClassName='App-nav-active' className='App-nav-link'>Book Journal</NavLink>
-			</nav>
-
-			<Switch>
-				<Route exact path='/book-browser'>
-					<BookBrowser />
-				</Route>
-				<Route exact path='/journal'>
-					<Journal />
-				</Route>
-				<Route exact path={'/book-browser/:bookId'}>
-					<BookDetails />
-				</Route>
-				<Route exact path={'/journal/:bookId'}>
-					<JournalEntryDetails />
-				</Route>
-				<Route exact path={'/journal/edit/:bookId'}>
-					<EditJournalEntry />
-				</Route>
-				<Redirect exact from='/' to='/book-browser' />
-			</Switch>
-		</div>
-	)
 }
 
-export default App
+export default withRouter(App)
--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -1,27 +1,30 @@
 // This is a display component used in the Journal component to display the books in the Journal.
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './JournalEntry.css'
 import useFormatAuthors from './hooks/useFormatAuthors'
 
-const JournalEntry = props => {
-    // Formatting needed if authors are missing or if there is more than one.
-    const authors = useFormatAuthors(props.book.bookAuthors)
+class JournalEntry extends Component {
+    render() {
+        const props = this.props
+        // Formatting needed if authors are missing or if there is more than one.
+        const authors = useFormatAuthors(props.book.bookAuthors)
 
-    return (
-        <div className='Journal-entry'>
-            <Link to={`journal/${props.book.bookId}`} className='Journal-entry-link'>
-                <div className='Journal-entry-container'>
-                    <img src={props.book.bookThumbnail} alt={props.book.bookTitle} className='Journal-entry-image'/>
-                    <div className='Journal-entry-description'>
-                        <h2>{props.book.bookTitle}</h2>
-                        {props.book.bookSubtitle && <div className='Journal-entry-subtitle'>{props.book.bookSubtitle}</div>}
-                        {authors}
+        return (
+            <div className='Journal-entry' >
+                <Link to={`journal/${props.book.bookId}`} className='Journal-entry-link'>
+                    <div className='Journal-entry-container'>
+                        <img src={props.book.bookThumbnail} alt={props.book.bookTitle} className='Journal-entry-image' />
+                        <div className='Journal-entry-description'>
+                            <h2>{props.book.bookTitle}</h2>
+                            {props.book.bookSubtitle && <div className='Journal-entry-subtitle'>{props.book.bookSubtitle}</div>}
+                            {authors}
+                        </div>
                     </div>
-                </div>
-            </Link>
-        </div>
-    )
+                </Link>
+            </div>
+        )
+    }
 }
 
 export default JournalEntry
